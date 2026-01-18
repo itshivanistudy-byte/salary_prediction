be5cad5cc6e4a7d23e01f0f5f3fa35491170c27c
itshivanistudy-byte <itshivani.study@gmail.com>
2026-01-18 13:28:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ job=st.selectbox("Job Title",encoders["Job Title"].classes_)
 experience=st.number_input("Years of Experience",0.0,40.0,2.0)
 
 #-----------CREATE DATAFRAME-------------
-data=pd.DataFrame({
+df=pd.DataFrame({
     "Age":[age],
     "Gender":[gender],
     "Education Level":[education],
